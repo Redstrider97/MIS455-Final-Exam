0e3834cfa28d390658b35cba45d3a50b8c98efb6
Redstrider97 <jbhaldar1234@gmail.com>
2021-12-24 11:50:22 +0600

    second commit

--- weather.js
+++ weather.js
@@ -1,63 +1,113 @@
-const currentWeatherItemsEl = document.getElementById('apiData');
+const currentWeatherDetails = document.getElementById('apiData');
 
 
 function process(){
-
     var typed = document.getElementById("category").value;
-
-    var link =`https://api.openweathermap.org/data/2.5/weather?q=${typed}&appid=9b92a815e8ac59826a4ab16c1f308f93`;
-
+    var link =`https://api.openweathermap.org/data/2.5/weather?q=${typed}&units=metric&appid=9b92a815e8ac59826a4ab16c1f308f93`;
     fetch (link)
     .then (res => res.json() )
-    .then (data => showResult (data) );
-   
-
+    .then (data => showSearchedResult (data) );
 }
 
-
-function showResult(data){
+function showSearchedResult(data){
     console.log (data.main);
 
+    var city = document.getElementById("category").value;
 let {humidity, feels_like, pressure, temp, temp_max, temp_min} = data.main;
+let {main} = data.weather[0];
 
-currentWeatherItemsEl.innerHTML = `
-        Feels Like: ${feels_like} <br>
-        Humidity:  ${humidity} <br>
-       Pressure: ${pressure} <br>
-       Temperature: ${temp} <br>
-       Maximum Temperature:  ${temp_max} <br>
-       Minimum Temperature:  ${temp_min} <br> 
-        ` ;
+currentWeatherDetails.innerHTML = `   
+<div class="card mx-auto" style="width: 18rem;">
+<img src="`+ weatherImg(main)+`" class="card-img-top" alt="...">
+<div class="card-body">
+    <h5 class="card-title text-center">${city}</h5>
+  <p class="card-text">Feels Like: ${feels_like} <br>
+               Humidity:  ${humidity} <br>
+             Pressure: ${pressure} <br>
+             Temperature: ${temp} <br>
+             Maximum Temperature:  ${temp_max} <br>
+             Minimum Temperature:  ${temp_min} <br>
+             Weather: ${main} </p></p>
+</div>
+</div>`;
+
+function weatherImg(main){
+    if(main=="Clear"){
+        image = "Images/clear.png"
+        return image;
+    }
+    else if(main =="Clouds"){
+        image = "/Images/clouds.png"
+        return image;
+    }
+    else if(main =="Mist"){
+        image = "/Images/mist.jpg"
+        return image;
+    }
+    else if(main =="Smoke" ){
+        image = "/Images/smoke.png"
+        return image;
+    }
+    else{
+        image="/Images/rain.svg"
+        return image;
+    }
+}
     
 
 }
 
-var url = `https://api.openweathermap.org/data/2.5/weather?q=Dhaka&appid=9b92a815e8ac59826a4ab16c1f308f93`;
+var url = `https://api.openweathermap.org/data/2.5/weather?q=Dhaka&units=metric&appid=9b92a815e8ac59826a4ab16c1f308f93`;
 
 function callAPI(){
     fetch (url)
     .then (res => res.json() )
-    .then (data => showDefault (data) );
+    .then (data => showByDefault (data) );
    
 }
 
 callAPI();
 
 
-function showDefault(data){
+function showByDefault(data){
     let {humidity, feels_like, pressure, temp, temp_max, temp_min} = data.main;
+    let {main} = data.weather[0];
 
-    currentWeatherItemsEl.innerHTML = `   
+    currentWeatherDetails.innerHTML = `   
     <div class="card mx-auto" style="width: 18rem;">
-    <img src="..." class="card-img-top" alt="...">
+    <img src="`+ weatherImg(main)+`" class="card-img-top" alt="...">
     <div class="card-body">
+    <h5 class="card-title text-center">Dhaka</h5>
       <p class="card-text">Feels Like: ${feels_like} <br>
                    Humidity:  ${humidity} <br>
                  Pressure: ${pressure} <br>
                  Temperature: ${temp} <br>
                  Maximum Temperature:  ${temp_max} <br>
-                 Minimum Temperature:  ${temp_min} <br></p></p>
+                 Minimum Temperature:  ${temp_min} <br>
+                 Weather: ${main}</p></p>
     </div>
     </div>`;
-       
+
+    function weatherImg(main){
+        if(main=="Clear"){
+            image = "Images/clear.png"
+            return image;
+        }
+        else if(main =="Clouds"){
+            image = "/Images/clouds.png"
+            return image;
+        }
+        else if(main =="Mist"){
+            image = "/Images/mist.jpg"
+            return image;
+        }
+        else if(main =="Smoke" ){
+            image = "/Images/smoke.png"
+            return image;
+        }
+        else{
+            image="/Images/rain.svg"
+            return image;
+        }
+    }
 }
